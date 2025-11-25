5694330761b6c5b7ab31bb5676721bb6f7e21aa1
ProssimalCrost <theylon.silva@hotmail.com>
2025-11-25 10:15:05 -0300

    feat: instalando Cors, configurando package.json, e tsconfig e craindo router

---
+++ routes/atendimentos.ts
@@ -0,0 +1,12 @@
+import  {Router} from 'express';
+
+
+const router = Router();
+
+//-- Rotas de Usuario --
+router.post('/atendimentos', (req, res) => {
+    res.send('Criar um novo atendimento')
+}
+
+
+ export {router};
