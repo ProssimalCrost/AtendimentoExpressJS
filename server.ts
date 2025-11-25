fb4dd72dd9db2553d82eed77cb5bb91ceb251c57
ProssimalCrost <theylon.silva@hotmail.com>
2025-11-25 10:32:37 -0300

    feat: iniciando o servidor no server.ts; configurando package.json para server.ts

--- server.ts
+++ server.ts
@@ -1,3 +1,16 @@
 import express from "express";
 import {router} from "./routes/atendimentos.ts";
 import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/atendimentos", router);
+
+app.listen(3000, () => {
+    console.log("Servidor rodando");
+})
+
+app.post("/test", (req, res ) => {
+    res.send("Teste Ok");
+})
