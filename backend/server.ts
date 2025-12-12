c4330236eab67a5bf0b9037a6a4efa551d59962e
ProssimalCrost <theylon.silva@hotmail.com>
2025-12-12 17:29:21 -0300

    Fix: criacao de nova Migration, e tentativa não sucedida de POST

--- backend/server.ts
+++ backend/server.ts
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/atendimentos", router); /*Ao usar "/atendimentos, router" as rotas em router devem conter apenas "/" */ 
 
-/* Comando para iniciar o srvidor: node --run dev */ 
+/* Comando para iniciar o srvidor: node --run dev http://localhost:3000/atendimentos */ 
 
 app.listen(3000, () => {
     console.log("Servidor rodando");
