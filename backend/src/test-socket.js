017e5a68909ba56ee716193b9227d3409ce54c3c
ProssimalCrost <theylon.silva@hotmail.com>
2025-12-26 11:08:52 -0300

    fix: colocando arquivos em producao detro de dist

--- backend/src/test-socket.js
+++
@@ -1,9 +0,0 @@
-import { io } from "socket.io-client";
-const socket = io("http://localhost:3000");
-socket.on("attendance:new", (data) => {
-    console.log("Novo atendimento:", data);
-});
-socket.on("attendance:finished", (data) => {
-    console.log("Atendimento finalizado:", data);
-});
-//# sourceMappingURL=test-socket.js.map
