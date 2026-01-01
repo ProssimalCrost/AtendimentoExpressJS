19460fbad6ebc549c64e1835348cbaa7185f454b
ProssimalCrost <theylon.silva@hotmail.com>
2026-01-01 16:13:02 -0300

    fix: corrige uso da variável NEXT_PUBLIC_SOCKET_URL no socket.io client

--- frontend/lib/socket.ts
+++ frontend/lib/socket.ts
@@ -1,5 +1,9 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3333", {
-  autoConnect: false,
-});
+export const socket = io(
+  process.env.NEXT_PUBLIC_SOCKET_URL!,
+  {
+    autoConnect: false,
+    transports: ["websocket"], // importante para Render
+  }
+);
