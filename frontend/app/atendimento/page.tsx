f7cf70549f8e250bdf87ba5f8c5ced375eb9e0bb
ProssimalCrost <theylon.silva@hotmail.com>
2026-02-24 11:38:16 -0300

    fx: removendo rota duplicada

--- frontend/app/atendimento/page.tsx
+++ frontend/app/atendimento/page.tsx
@@ -16,7 +16,7 @@ export default function AtendimentosPage() {
 async function loadAtendimentos() {
   try {
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/atendimentos`
+      `${process.env.NEXT_PUBLIC_API_URL}`
     );
 
     if (!response.ok) return;
