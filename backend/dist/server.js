93ab222819135ee586654f7f66abecf75610f86b
ProssimalCrost <theylon.silva@hotmail.com>
2026-02-24 16:41:47 -0300

    alterando a porta do servidor, dpurando codigo com build

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -4,27 +4,30 @@ import { router } from "./routes/atendimentos.js";
 import cors from "cors";
 import http from "http";
 import { Server as IOServer } from "socket.io";
+// é necessario import "dotenv/config" 
+// no topo deste arquivo para funcionar a variavel de ambiente .env
 const app = express();
 app.use(express.json({ type: "*/*" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: [
-        // "http://localhost:3000",
+        "http://localhost:3000",
         "https://atendimento-express.vercel.app",
     ],
     methods: ["GET", "POST", "PATCH", "OPTIONS"],
     credentials: true,
 }));
 /* Comando para iniciar o srvidor: node --run dev http://localhost:3000/atendimentos */
-const httpServer = http.createServer(app);
-const io = new IOServer(httpServer, {
+const server = http.createServer(app);
+const io = new IOServer(server, {
     cors: {
-        origin: [
-            // "http://localhost:3000",
-            "https://atendimento-express.vercel.app"
-        ],
-        methods: ["GET", "POST", "PATCH", "OPTIONS"],
-    }
+        origin: "*",
+        methods: ["GET", "POST", "PATCH"],
+    },
+    transports: ["websocket"], // 🔥 IMPORTANTE
+});
+io.on("connection", (socket) => {
+    console.log("✅ Cliente conectado:", socket.id);
 });
 io.on("connection", (socket) => {
     console.log("Cliente conectado:", socket.id);
@@ -32,9 +35,7 @@ io.on("connection", (socket) => {
 export { io };
 app.use("/atendimentos", router); /*Ao usar "/atendimentos, router" as rotas em router devem conter apenas "/" */
 const PORT = process.env.PORT || 3333;
-httpServer.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
-// é necessario import "dotenv/config" 
-// no topo deste arquivo para funcionar a variavel de ambiente .env
 //# sourceMappingURL=server.js.map
