39ebdcff7f27013673489ad5a6b990841a71fd96
ProssimalCrost <theylon.silva@hotmail.com>
2026-02-26 11:23:56 -0300

    Alteração da variavel de ambiente

--- frontend/lib/socket.ts
+++ frontend/lib/socket.ts
@@ -10,8 +10,3 @@ export const socket = io(
     reconnectionDelay: 1000,
   }
 );
-<<<<<<< Updated upstream
-=======
-// Verificar se ultiliza o socket
-// socket na oconecta
->>>>>>> Stashed changes
