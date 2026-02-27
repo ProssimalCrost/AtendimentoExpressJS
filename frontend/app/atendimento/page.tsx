66be68cc793d2e10355e79f65bfd111366fea302
ProssimalCrost <theylon.silva@hotmail.com>
2026-02-27 10:35:54 -0300

    comentarios em atendimentos

--- frontend/app/atendimento/page.tsx
+++ frontend/app/atendimento/page.tsx
@@ -14,6 +14,7 @@ export default function AtendimentosPage() {
   const [atendimentos, setAtendimentos] = useState<Atendimento[]>([]);
   const lastCountRef = useRef(0);
 
+  ////   CARREGA ATENDIMENTOS //// 
   async function loadAtendimentos() {
     const res = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}`,
