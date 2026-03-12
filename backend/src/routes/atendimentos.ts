16c92114c6258a09dc3dbc5a495b9ff66972ed00
ProssimalCrost <theylon.silva@hotmail.com>
2026-03-12 17:32:58 -0300

    fix: corrigindo import controller

--- backend/src/routes/atendimentos.ts
+++ backend/src/routes/atendimentos.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
-import AttendimentoController from '../controllers/atendimentoController.js';
+import {AtendimentosController} from '../controllers/atendimentoController';
 
 const router = Router();
 
 //-- Rotas de Usuario --
-router.post('/', (req, res) => AttendimentoController.create(req, res));
+router.post('/', (req, res) => AtendimentosController.create(req, res));
 
-router.get('/', (req, res) => AttendimentoController.list(req, res)) 
+router.get('/', (req, res) => AtendimentosController.list(req, res)) 
 
-router.patch('/:id/finish', (req, res) => AttendimentoController.finish(req, res))
+router.patch('/:id/finish', (req, res) => AtendimentosController.finish(req, res))
 
 export  { router };
